17daaf82867d37aa5b36a7333e9f95419a4398c0
Hiba <hibahibz666@gmail.com>
2024-08-28 13:57:16 +0530

    bom last

--- bill_of_material_cart/__manifest__.py
+++ bill_of_material_cart/__manifest__.py
@@ -7,18 +7,18 @@
     'description': "Bill Of Material in Cart",
     'data': [
      'views/res_config_views.xml',
-      # 'views/website_sale_view.xml'
+      'views/website_sale_view.xml'
 
 
     ],
-'assets': {
-    'web.assets_frontend': [
-        'bill_of_material_cart/static/src/js/product_bom.js',
-    ]
+# 'assets': {
+#     'web.assets_frontend': [
+#         'bill_of_material_cart/static/src/js/product_bom.js',
+#     ]
 # 'web.assets_qweb': [
 #         'bill_of_material_cart/static/src/xml/website_sale_view.xml',
 #     ],
-}
+# }
 
 
 }
--- hospital/model/hospital_doctors.py
+++ hospital/model/hospital_doctors.py
@@ -21,3 +21,94 @@ class HrEmployee(models.Model):
     #             browse(post.get('id')))
     #     return request.render('hostel_management.room_data_snippet',
     #                          {'room':room})
+
+#     / **
+#
+#     @odoo - module * /
+#
+#     import publicWidget
+#     from
+#     "@web/legacy/js/public/public_widget";
+#     import
+#     {jsonrpc}
+#     from
+#     "@web/core/network/rpc_service";
+#
+#     publicWidget.registry.ProductBoM = publicWidget.Widget.extend({
+#         selector: '.oe_website_sale',
+#
+#         start: function() {
+#             this._super.apply(this, arguments);
+#     console.log('Widget initialized');
+#     this._getBomDetails();
+#     },
+#
+#     _getBomDetails: function()
+#     {
+#         console.log('Fetching BoM details');
+#     // Find
+#     all
+#     products in the
+#     cart
+#     const $products = this.$el.find("#cart_products .product");
+#
+#     // Iterate
+#     over
+#     each
+#     product and fetch
+#     its
+#     BoM
+#     $products.each((index, element) = > {
+#         const
+#     productId = $(element).data('product-id'); // Get
+#     product
+#     ID
+#     from the element
+#     if (productId) {
+#     jsonrpc('/get_product_bom', {params: {product_id: productId}})
+#     .then(result= > {
+#     if (result.bom_data & & result.bom_data.length)
+#     {
+#         this._displayBoMData(productId, result.bom_data, $(element));
+#     }
+#     });
+#     }
+#     });
+#     },
+#
+#     _displayBoMData: function(productId, bomData, $productElement) {
+#         const $bomContainer = $('<div/>', {
+#
+#     class: 'product_bom_data'
+#
+#     });
+#     console.log('Displaying BoM data for product ID:', productId);
+#
+#     bomData.forEach(bom= > {
+#     $bomContainer.append(`
+#                          < div
+#
+#     class ="bom_item" >
+#
+#     < strong > Product: < / strong > ${bom.name} < br >
+#     < strong > Quantity: < / strong > ${bom.quantity}
+#
+# < / div >
+# `);
+# });
+#
+# // Append
+# BoM
+# data
+# after
+# the
+# product
+# description in the
+# cart
+# $productElement.find('.product_description').after($bomContainer);
+# },
+# });
+#
+# export
+# default
+# publicWidget.registry.ProductBoM;
