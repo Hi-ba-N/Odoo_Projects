0ffa971ff83ef55538d9e334c4b123ba18644e2a
Hiba <hibahibz666@gmail.com>
2024-08-28 18:07:51 +0530

    bom5

--- bill_of_material_cart/__manifest__.py
+++ bill_of_material_cart/__manifest__.py
@@ -11,14 +11,7 @@
 
 
     ],
-# 'assets': {
-#     'web.assets_frontend': [
-#         'bill_of_material_cart/static/src/js/product_bom.js',
-#     ]
-# 'web.assets_qweb': [
-#         'bill_of_material_cart/static/src/xml/website_sale_view.xml',
-#     ],
-# }
+
 
 
 }
