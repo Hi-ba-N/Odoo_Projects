22c272d954e69048cdfbf29c45408d0323167c6d
Hiba <hibahibz666@gmail.com>
2024-09-06 17:32:45 +0530

    payment tt

--- payment_payu/models/payment_provider.py
+++ payment_payu/models/payment_provider.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import hashlib
+
 from odoo import fields, models
 
 
@@ -12,6 +14,9 @@ class PaymentProvider(models.Model):
         string="Merchant Key",
         required_if_provider="payu"
     )
+    # payu_merchant_salt = fields.Char(
+    #     string="Merchant Salt", required_if_provider='payu'
+    #     )
 
     def _get_supported_currencies(self):
         """ Override of `payment` to return INR as the only supported currency. """
@@ -22,5 +27,24 @@ class PaymentProvider(models.Model):
                 lambda c: c.name == 'INR')
         print('kk', supported_currencies)
         return supported_currencies
+
+
+    def _payu_generate_sign(self, values, incoming=True):
+        """ Generate the hash for incoming or outgoing communications."""
+        sign_values = {
+            **values,
+            'key': self.payu_merchant_key,
+        }
+        if incoming:
+            print('incoming')
+            keys = 'salt|status||||||udf5|udf4|udf3|udf2|udf1|email|firstname|productinfo|amount|' \
+                   'txnid|key'
+            sign = '|'.join(f'{sign_values.get(k) or ""}' for k in keys.split('|'))
+        else:
+            print('else')
+            keys = 'key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5||||||salt'
+            sign = '|'.join(f'{sign_values.get(k) or ""}' for k in keys.split('|'))
+        return hashlib.sha512(sign.encode('utf-8')).hexdigest()
+
 
 
--- payment_payu/models/payment_transaction.py
+++ payment_payu/models/payment_transaction.py
@@ -1,71 +1,97 @@
-from odoo import models
+
+from odoo import _ , models
+from odoo.exceptions import ValidationError
+
+
+# from odoo.addons.payment import utils as payment_utils
 
 
 class PaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
 
     def _get_specific_rendering_values(self, processing_values):
-        print(processing_values)
         res = super()._get_specific_rendering_values(processing_values)
+        print(processing_values)
+        api_url = 'https://secure.payu.in/_payment'
         if self.provider_code != 'payu':
             return res
         payu_values = {
-            'key': self.provider_id.payumoney_merchant_key,
-            'txnid': '',
-            'firstname': self.partner_name,
+            'key': self.provider_id.payu_merchant_key,
+            'txnid': self.reference,
+            'productinfo': self.reference,
+            'firstname': self.partner_id.name,
             'amount': self.amount,
             'email': self.partner_email,
             'phone': self.partner_phone,
+            'surl': 'https://apiplayground-response.herokuapp.com/',
+            'furl': 'https://apiplayground-response.herokuapp.com/',
+            'api_url': api_url,
+
+
         }
+        payu_values['hash'] = self.provider_id._payu_generate_sign(
+            payu_values, incoming=False,
+        )
+        print(payu_values)
+        print(payu_values['hash'])
+        return payu_values
 
+    def _get_tx_from_notification_data(self, provider_code,
+                                       notification_data):
+        """ Override of payment to find the transaction based on Payu data."""
 
+        tx = super()._get_tx_from_notification_data(provider_code,
+                                                    notification_data)
+        print(notification_data)
+        if provider_code != 'payu' or len(tx) == 1:
+            return tx
 
+        reference = notification_data.get('txnid')
+        if not reference:
+            raise ValidationError(
+                "PayU: " + _(
+                    "Received data with missing reference (%s)", reference)
+            )
 
+        tx = self.search([('reference', '=', reference),
+                          ('provider_code', '=', 'payu')])
 
+        if not tx:
+            raise ValidationError(
+                "PayU: " + _(
+                    "No transaction found matching reference %s.",
+                    reference)
+            )
 
+        return tx
 
+    def _process_notification_data(self, notification_data):
+        """ Override of payment to process the transaction based on Payu data.
 
+        Note: self.ensure_one()
 
+        :param dict notification_data: The notification data sent by the provider
+        :return: None
+        """
+        super()._process_notification_data(notification_data)
+        if self.provider_code != 'payu':
+            return
 
+        # Update the provider reference.
+        self.provider_reference = notification_data.get('payuId')
 
+        # Update the payment method
+        payment_method_type = notification_data.get('bankcode', '')
+        payment_method = self.env['payment.method']._get_from_code(payment_method_type)
+        self.payment_method_id = payment_method or self.payment_method_id
 
+        # Update the payment state.
+        status = notification_data.get('status')
+        if status == 'success':
+            self._set_done()
+        else:
+            error_code = notification_data.get('Error')
+            self._set_error(
+                "PayU: " + _("The payment encountered an error with code %s", error_code)
+            )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # res = super()._get_specific_rendering_values(processing_values)
-        # if self.provider_code != 'payu':
-        #     return res
-        # first_name = self.partner_id.name
-        # payu_values = {
-        #     'key': self.provider_id.payu_merchant_key,
-        #     'txnid': self.reference,
-        #     'amount': self.amount,
-        #     'productinfo': self.reference,
-        #     'firstname': first_name,
-        #     'email': self.partner_email,
-        #     'phone': self.partner_phone
-        #
-        # }
