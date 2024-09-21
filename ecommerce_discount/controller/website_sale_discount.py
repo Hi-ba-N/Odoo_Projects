05802448268bc066fda31225278265bb8a6a2745
Hiba <hibahibz666@gmail.com>
2024-09-21 09:13:51 +0530

    today

--- ecommerce_discount/controller/website_sale_discount.py
+++ ecommerce_discount/controller/website_sale_discount.py
@@ -16,22 +16,30 @@ class WebsiteSaleDiscount(WebsiteSale):
                 ('active', '=', True)
             ], limit=1)
             if promotion_program:
-                discount_percentage = 5.0
-                total_discount = (order.amount_total * discount_percentage) / 100
+                print(order.amount_total)
+                discount_value = promotion_program.reward_ids.discount
+                total_discount = (order.amount_total * discount_value) / 100
                 print(total_discount)
 
-                # Apply the discount directly to the total amount
                 order.write({
                     'amount_total': order.amount_total - total_discount
                 })
+                print(order.amount_total)
 
-                # Display message in green for successful application of the promotion
                 result.qcontext['promotion_message'] = "Promotion applied"
-                result.qcontext['promotion_message_class'] = "text-success"  # Bootstrap class for green text
 
-                # Return the updated cart view with the message
             print(result.qcontext)
             return result
+
+
+
+
+
+
+
+
+
+
 
         #         print(order.amount_total)
         #         discount_value = promotion_program.reward_ids.discount
